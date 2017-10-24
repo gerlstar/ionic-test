e3b89ad328f1bf7c4b6cbccffbbe77435fe2d68c
Vanessa Villanueva <vvillanueva@ci.com>
2017-10-24 10:24:20 -0400

    removed unnecessary validation for email. Added onSubmit functionality for login component

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {FormBuilder, FormGroup, Validators, AbstractControl} from '@angular/forms';
+import {NavController, NavParams} from 'ionic-angular';
 
-import { HelloIonicPage } from '../../pages/hello-ionic/hello-ionic';
+import {HelloIonicPage} from '../../pages/hello-ionic/hello-ionic';
+import {LoginSuccessPage} from "../../pages/login-success/login-success";
+import {AuthService} from "../../shared/auth-service/auth-service";
 
-@IonicPage()
 @Component({
     selector: 'page-login',
     templateUrl: 'login.html',
@@ -14,22 +15,38 @@ export class LoginPage {
 
     authForm: FormGroup;
 
-    constructor(public nav: NavController, public navParams: NavParams, public formBuilder: FormBuilder) {
+    constructor(private authService: AuthService, public nav: NavController, public navParams: NavParams,
+                public formBuilder: FormBuilder) {
 
         this.nav = nav;
 
+        // this.authForm = formBuilder.group({
+        //     username: ['', Validators.compose([Validators.required, Validators.pattern('[a-zA-Z]*'), Validators.minLength(8), Validators.maxLength(30)])],
+        //     password: ['', Validators.compose([Validators.required, Validators.minLength(8)])]
+        // });
+
         this.authForm = formBuilder.group({
-            username: ['', Validators.compose([Validators.required, Validators.pattern('[a-zA-Z]*'), Validators.minLength(8), Validators.maxLength(30)])],
-            password: ['', Validators.compose([Validators.required, Validators.minLength(8)])]
+            email: [''],
+            password: ['']
         });
     }
 
-    onSubmit(value: any): void {
-        if(this.authForm.valid) {
-            window.localStorage.setItem('username', value.username);
-            window.localStorage.setItem('password', value.password);
+    onSubmit(credentials: any): void {
+        // if(this.authForm.valid) {
+        //     window.localStorage.setItem('username', value.username);
+        //     window.localStorage.setItem('password', value.password);
+        this.authService.login(credentials)
+            .subscribe(result => {
+                if (result)
+                // this.router.navigate(['/login-success']);
+                    this.nav.push(LoginSuccessPage);
+                else{
+                    console.error('error..');
+                }
+                // this.invalidLogin = true;
+            });
 
-            this.nav.push(HelloIonicPage);
-        }
+
+        // }
     }
 }

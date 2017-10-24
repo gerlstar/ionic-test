8861435557dfc842b79324ff84d2182e9e1cb08d
Vanessa Villanueva <vvillanueva@ci.com>
2017-10-24 10:22:12 -0400

    imported http, base request, forms, reactive forms, mockbackend, fakebackend provider, auth service and login success page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,12 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule, ErrorHandler} from '@angular/core';
+import {HttpModule, BaseRequestOptions} from '@angular/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { MockBackend } from '@angular/http/testing';
 import {IonicApp, IonicModule, IonicErrorHandler} from 'ionic-angular';
+
+import { fakeBackendProvider } from '../shared/helpers/fake-backend';
+
 import {MyApp} from './app.component';
 
 import {HelloIonicPage} from '../pages/hello-ionic/hello-ionic';
@@ -10,6 +16,8 @@ import {LoginPage} from '../pages/login/login';
 
 import {StatusBar} from '@ionic-native/status-bar';
 import {SplashScreen} from '@ionic-native/splash-screen';
+import {AuthService} from "../shared/auth-service/auth-service";
+import {LoginSuccessPage} from "../pages/login-success/login-success";
 
 @NgModule({
     declarations: [
@@ -17,9 +25,12 @@ import {SplashScreen} from '@ionic-native/splash-screen';
         HelloIonicPage,
         ItemDetailsPage,
         ListPage,
-        LoginPage
+        LoginPage,
+        LoginSuccessPage
     ],
     imports: [
+        HttpModule,
+        FormsModule,
         BrowserModule,
         IonicModule.forRoot(MyApp),
     ],
@@ -29,12 +40,18 @@ import {SplashScreen} from '@ionic-native/splash-screen';
         HelloIonicPage,
         ItemDetailsPage,
         ListPage,
-        LoginPage
+        LoginPage,
+        LoginSuccessPage
     ],
     providers: [
         StatusBar,
         SplashScreen,
-        {provide: ErrorHandler, useClass: IonicErrorHandler}
+        AuthService,
+        {provide: ErrorHandler, useClass: IonicErrorHandler},
+        // For creating a mock back-end. You don't need these in a real app.
+        fakeBackendProvider,
+        MockBackend,
+        BaseRequestOptions
     ]
 })
 export class AppModule {

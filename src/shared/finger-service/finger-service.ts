f592bc37da7324ab4531cd2bd7cbe97a22d9766a
Vanessa Villanueva <vvillanueva@ci.com>
2017-11-21 16:09:35 -0500

    temporary commented out fingerprint aio functionality in finger service

--- src/shared/finger-service/finger-service.ts
+++ src/shared/finger-service/finger-service.ts
@@ -1,45 +1,50 @@
-import {FingerprintAIO, FingerprintOptions} from '@ionic-native/fingerprint-aio';
+// import {FingerprintAIO, FingerprintOptions} from '@ionic-native/fingerprint-aio';
 import { Injectable } from '@angular/core';
 import {LoginSuccessPage} from "../../pages/login-success/login-success";
 import {NavController, NavParams, App,Platform} from 'ionic-angular';
 
 @Injectable()
 export class FingerService{
-    fingerprintOptions: FingerprintOptions;
+    // fingerprintOptions: FingerprintOptions;
     nav: NavController;
 
-    constructor(private app:App, private fingerAuth: FingerprintAIO) {
+    // constructor(private app:App, private fingerAuth: FingerprintAIO) {
+    //     console.log('Hello FingerAuthComponent Component');
+
+    // }
+    constructor(private app:App) {
         console.log('Hello FingerAuthComponent Component');
         // this.nav = nav;
         // this.nav = app.getActiveNav();
     }
 
     public showFingerprintAuthDlg() {
-        this.fingerprintOptions = {
-            clientId: 'fingerprint-Demo',
-            clientSecret: 'password', //Only necessary for Android
-            disableBackup: true  //Only for Android(optional)
-        }
-        return this.fingerAuth.isAvailable().then(result => {
-            if (result === "OK") {
-                return this.fingerAuth.show(this.fingerprintOptions)
-                    .then((result: any) => {
-                        console.log(result);
-                        alert('success')
-                        localStorage.setItem('fingerPrintToken', result);
-                        return result.withFingerprint;
-
-                    })
-                    .catch((error: any) => {
-                        console.log(error);
-                        alert('error!');
-                        // alert(JSON.stringify(error));
-                        alert(JSON.stringify(error, Object.getOwnPropertyNames(error)));
-                        return false;
-                    });
-            }else{
-                return false;
-            }
-        });
+        console.info('show finger print auth dlg');
+        // this.fingerprintOptions = {
+        //     clientId: 'fingerprint-Demo',
+        //     clientSecret: 'password', //Only necessary for Android
+        //     disableBackup: true  //Only for Android(optional)
+        // }
+        // return this.fingerAuth.isAvailable().then(result => {
+        //     if (result === "OK") {
+        //         return this.fingerAuth.show(this.fingerprintOptions)
+        //             .then((result: any) => {
+        //                 console.log(result);
+        //                 alert('success')
+        //                 localStorage.setItem('fingerPrintToken', result);
+        //                 return result.withFingerprint;
+        //
+        //             })
+        //             .catch((error: any) => {
+        //                 console.log(error);
+        //                 alert('error!');
+        //                 // alert(JSON.stringify(error));
+        //                 alert(JSON.stringify(error, Object.getOwnPropertyNames(error)));
+        //                 return false;
+        //             });
+        //     }else{
+        //         return false;
+        //     }
+        // });
     }
 }

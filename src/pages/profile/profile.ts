2b999e19fc30f1dc84d17993f99fb3c5b1930de6
Vanessa Villanueva <vvillanueva@ci.com>
2017-11-27 11:49:00 -0500

    added onInit function in profile.ts to initialize isLoggedIn variable

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {AuthService} from "../../shared/auth-service/auth-service";
 
@@ -14,14 +14,23 @@ import {AuthService} from "../../shared/auth-service/auth-service";
   selector: 'page-profile',
   templateUrl: 'profile.html',
 })
-export class ProfilePage {
+export class ProfilePage implements OnInit{
+
+    isLoggedIn = false;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public authService:AuthService) {
       // this.navCtrl.pop();
+      // this.isLoggedIn = this.authService.isLoggedIn();
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ProfilePage');
+    // console.log('ionViewDidLoad ProfilePage');
   }
+
+  ngOnInit(){
+      // alert("profile on int");
+      this.isLoggedIn = this.authService.isLoggedIn();
+  }
+
 
 }

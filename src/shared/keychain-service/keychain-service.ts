98250855b111bb3175461477e5fc903ec791308a
Vanessa Villanueva <vvillanueva@ci.com>
2017-11-27 11:50:42 -0500

    added use of variables when checking if we have the keychain set up already

--- src/shared/keychain-service/keychain-service.ts
+++ src/shared/keychain-service/keychain-service.ts
@@ -7,7 +7,7 @@ import { KeychainTouchId } from '@ionic-native/keychain-touch-id';
 @Injectable()
 export class KeychainService{
     nav: NavController;
-
+    hasKeyChain = false;
 
     constructor(private app:App,
                 private keychainTouchId: KeychainTouchId) {
@@ -15,29 +15,48 @@ export class KeychainService{
 
     }
 
+    public getHasKeychain(){
+        return this.hasKeyChain;
+    }
 
     public showFingerPrintDlg(keychain:string){
         //first checks if the finger print functionality is available on the device
         return this.keychainTouchId.isAvailable()
             .then((res: any) => {
-                // alert(res);
-                alert('finger print func available!');
-                return  this.hasKey(keychain)
-                    .then((res:any) => {
-                        //the key doesnt exist so lets open the finger print dialog!
-                        if (res == false){
-                            alert('Failed! Opening the finger print dialog now');
-                            this.displayFingerPrint(keychain)
-                                .then((res:any) => {
-                                    // alert('res ='+res);
-                                    return res;
-                                });
-                        }else{
-                            // alert('key already exists!')
-                            //key already exists
-                            return false;
-                        }
-                    });
+                alert('showFingerPrintDlg = '+this.hasKeyChain);
+                // alert('finger print func available!');
+                if (this.hasKeyChain == false){
+                    return this.displayFingerPrint(keychain)
+                                    .then((res:any) => {
+                                        alert('res ='+res);
+                                        if (res){
+                                            this.hasKeyChain = true;
+                                        }
+                                        return res;
+                                    });
+
+                    // return this.hasKeyChain;
+                    // return  this.hasKey(keychain)
+                    //     .then((res:any) => {
+                    //         //the key doesnt exist so lets open the finger print dialog!
+                    //         if (res == false){
+                    //             // alert('Failed! Opening the finger print dialog now');
+                    //             this.displayFingerPrint(keychain)
+                    //                 .then((res:any) => {
+                    //                     // alert('res ='+res);
+                    //                     return res;
+                    //                 });
+                    //         }else{
+                    //             // alert('key already exists!')
+                    //             //key already exists
+                    //             return false;
+                    //         }
+                    //     });
+                }else{
+                    alert('Keychain is true');
+                    return true;
+                }
+
             })
             .catch((error: any) => {
                 alert('error!');
@@ -64,7 +83,7 @@ export class KeychainService{
     }
 
 
-    hasKey(keychain:string){
+    public hasKey(keychain:string){
         // ionicKey = 'IONIC_KEY';
         // this.keychainTouchId.verify(ionicKey, 'VANESSA')
         //     .then((res:any) => {
@@ -80,25 +99,45 @@ export class KeychainService{
 
         return this.keychainTouchId.has(keychain)
             .then((res:any) => {
-                alert(res);
-                alert('This key ' + keychain + ' already exists!');
+                alert('result = '+res);
+                // alert('This key ' + keychain + ' already exists!');
                 // this.showConfirm();
+                this.hasKeyChain = true;
                 return true;
-            })
-            .catch((error: any) => {
+            },(error: any) => {
                 //secret key not available
-
-                alert('No password stored within the '+ keychain + ' key.');
+                this.hasKeyChain = false;
+                alert('No password stored within the ' + keychain + ' key.');
                 alert(error);
                 return false;
             });
+
+        // return this.keychainTouchId.has(keychain)
+        //     .then((res:any) => {
+        //         alert('result = '+res);
+        //         // alert('This key ' + keychain + ' already exists!');
+        //         // this.showConfirm();
+        //         this.hasKeyChain = true;
+        //         return true;
+        //     })
+        //     .catch((error: any) => {
+        //         //secret key not available
+        //         this.hasKeyChain = false;
+        //         alert('No password stored within the '+ keychain + ' key.');
+        //         alert(error);
+        //         return false;
+        //     });
     }
 
     public deleteKeyChain(keychain:string){
         return this.keychainTouchId.delete(keychain)
             .then((res:any) => {
+                this.hasKeyChain = false;
                 alert('Success in deleting the key');
                 return true;
+            },(err:any) => {
+                alert('Failed to delete the key');
+                return false;
             });
 
     }

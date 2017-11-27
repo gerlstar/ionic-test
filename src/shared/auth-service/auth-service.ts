83fe5079a420a8ad81b475c145d10c684ba9e92f
Vanessa Villanueva <vvillanueva@ci.com>
2017-11-27 11:49:53 -0500

    added events module in Auth service to handle the  logging in process

--- src/shared/auth-service/auth-service.ts
+++ src/shared/auth-service/auth-service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import { Events } from 'ionic-angular';
 import 'rxjs/add/operator/map';
 import { JwtHelper, tokenNotExpired } from 'angular2-jwt';
+import {KeychainService} from '../../shared/keychain-service/keychain-service';
 
 @Injectable()
 export class AuthService {
+    ionicKey = 'IONIC_KEY';
+
     // public currentUser :object;
-    constructor(private http: Http) {
+    constructor(private http: Http, private keychainService: KeychainService, public events:Events) {
     }
 
     login(credentials) {
@@ -17,8 +21,6 @@ export class AuthService {
                 const result = response.json();
                 console.log(result);
                 if (result && result.token){
-
-
                     localStorage.setItem('token', result.token);
                     return true;
                 }else{
@@ -27,15 +29,26 @@ export class AuthService {
             });
     }
 
+    //logout using local storage; commented temporarily
+    // logout() {
+    //     localStorage.removeItem('token');
+    //
+    //     const fingerPrintToken = localStorage.getItem('fingerPrintToken');
+    //     if (fingerPrintToken){
+    //         localStorage.removeItem('fingerPrintToken');
+    //     }
+    //
+    //     console.clear();
+    // }
+
+
     logout() {
-        localStorage.removeItem('token');
-
-        const fingerPrintToken = localStorage.getItem('fingerPrintToken');
-        if (fingerPrintToken){
-            localStorage.removeItem('fingerPrintToken');
-        }
-
-        console.clear();
+        this.keychainService.deleteKeyChain(this.ionicKey)
+            .then((res:any) => {
+                alert('delete key chain result ='+ res);
+            },(err:any) => {
+                alert('failed to delete key');
+            });
     }
 
     get currentUser(){
@@ -61,29 +74,32 @@ export class AuthService {
     isLoggedIn() {
 
         const regLogin = tokenNotExpired();
-        const fingerPrintToken = localStorage.getItem('fingerPrintToken');
+        //@todo temp removed so that i can use keychain instead of localstorage
+         // const fingerPrintToken = localStorage.getItem('fingerPrintToken');
+        // const regLogin = false;
+        // const fingerPrintToken = this.keychainService.hasKey(this.ionicKey);
         if (regLogin){
             return true;
         }else{
-            if (fingerPrintToken){
-                return true;
-            }else{
+            // const fingerPrintToken = this.keychainService.hasKey(this.ionicKey);
+
+            const s = this.keychainService.getHasKeychain();
+            // alert('has keychain? '+s);
+
+            if (s == false){
                 return false;
+            }else{
+                this.events.publish('isLoggedIn',true);
+                return true;
             }
+            // if (fingerPrintToken){
+            //     return true;
+            // }else{
+            //     return false;
+            // }
 
         }
 
-        // let jwtHelper = new JwtHelper();
-        // let token = localStorage.getItem('token');
-        // if (!token)
-        //     return false;
-        //
-        // let expireDate = jwtHelper.getTokenExpirationDate(token);
-        // console.log('expireDate=',expireDate);
-        // let isExpired = jwtHelper.isTokenExpired(token);
-        // console.log('isExpired=',isExpired);
-        //
-        // return !isExpired;
     }
 }
 
